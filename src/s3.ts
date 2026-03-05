91b1e723d711fdd5b0207b3f8e6115f629aa8517
Fadyy22 <fadyalaa441@gmail.com>
2026-03-05 10:09:40 +0200

    feat: add env for virtual hosted style

--- src/s3.ts
+++ src/s3.ts
@@ -1,15 +1,17 @@
-import { s3 } from 'bun'
-import { presignExpiresIn } from './config'
+import { s3 } from 'bun';
+import { presignExpiresIn } from './config';
 
 export async function checkObjectExists(key: string) {
-  const exists = await s3.exists(key, { virtualHostedStyle: true })
-  return exists
+  const exists = await s3.exists(key, {
+    virtualHostedStyle: process.env.S3_VIRTUAL_HOSTED_STYLE === 'true'
+  });
+  return exists;
 }
 
 export function generatePresignedUrl(key: string) {
   const url = s3.presign(key, {
-    virtualHostedStyle: true,
-    expiresIn: presignExpiresIn(),
-  })
-  return url
+    virtualHostedStyle: process.env.S3_VIRTUAL_HOSTED_STYLE === 'true',
+    expiresIn: presignExpiresIn()
+  });
+  return url;
 }
