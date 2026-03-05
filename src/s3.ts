02b0e5da8d0a9f199e03cdc0dee2847d8df83768
Fadyy22 <fadyalaa441@gmail.com>
2026-03-05 10:52:08 +0200

    chore: set S3_VIRTUAL_HOSTED_STYLE=false

--- src/s3.ts
+++ src/s3.ts
@@ -4,7 +4,7 @@ import { presignExpiresIn } from './config';
 export async function checkObjectExists(key: string) {
   const decodedKey = decodeURIComponent(key);
   const exists = await s3.exists(decodedKey, {
-    virtualHostedStyle: process.env.S3_VIRTUAL_HOSTED_STYLE === 'true'
+    virtualHostedStyle: true
   });
   return exists;
 }
@@ -12,7 +12,7 @@ export async function checkObjectExists(key: string) {
 export function generatePresignedUrl(key: string) {
   const decodedKey = decodeURIComponent(key);
   const url = s3.presign(decodedKey, {
-    virtualHostedStyle: process.env.S3_VIRTUAL_HOSTED_STYLE === 'true',
+    virtualHostedStyle: true,
     expiresIn: presignExpiresIn()
   });
   return url;
